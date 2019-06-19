3369a11804e6fab042fd53c2e77890f13512a184
Vinicius Oliveira <vr.sena@gmail.com>
2019-06-19 15:44:36 -0400

    Pusing blocks (incomplete)

---
+++ Assets/Scripts/PlayerPush.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPush : MonoBehaviour
+{
+    public float fltPushPower = 2f;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rbBody = hit.collider.attachedRigidbody;
+
+        if (rbBody == null || rbBody.isKinematic || hit.moveDirection.y < -0.3f) {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+        rbBody.velocity = pushDir * fltPushPower;
+    }
+}
