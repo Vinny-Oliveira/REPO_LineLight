c919fb17a355cbc8be445a3ce8be0f93aa76a508
Vinicius Oliveira <vr.sena@gmail.com>
2019-06-19 14:15:03 -0400

    Player moves around the plane.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float fltSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * fltSpeed, 0, Input.GetAxis("Vertical") * fltSpeed);
+    }
+}
