e8ad1f88165da887985047f9c0f05496de936e66
Vinicius Oliveira <vr.sena@gmail.com>
2019-06-20 10:59:10 -0400

    Blocks may be pushed

---
+++ Assets/Scripts/BlockKinematics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockKinematics : MonoBehaviour
+{
+    /// <summary>
+    /// Allow the block to move when the player is touching it
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log("Collided");
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    /// <summary>
+    /// Do not allow the block to move if the player is not touching it
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log("Separated");
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
